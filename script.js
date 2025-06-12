8037f505ada1faf9d10b8d7510869a8ec0a05091
Navediqbal123 <navedahmad9012@gmail.com>
2025-06-13 00:54:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+function processImage() {
+    const input = document.getElementById('fileInput');
+    const link = document.getElementById('downloadLink');
+    const preview = document.getElementById('preview');
 
-console.log("Pixvibe Loaded");
+    if (input.files.length === 0) {
+        alert("Please select a file first.");
+        return;
+    }
+
+    const file = input.files[0];
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        const img = new Image();
+        img.src = event.target.result;
+        img.style.maxWidth = '100%';
+        preview.innerHTML = "";
+        preview.appendChild(img);
+
+        link.href = event.target.result;
+        link.style.display = 'inline-block';
+    }
+    reader.readAsDataURL(file);
+}
